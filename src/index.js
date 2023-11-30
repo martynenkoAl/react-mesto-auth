609e6870863dd2caaa2889a5deb7375d1f9bbe5a
Alina Martynenko <martynenko.av.94@mail.ru>
2023-11-30 23:04:18 +1000

    add basename

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "../src/components/App.js";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from '../src/components/App.js';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/react-mesto-auth/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
